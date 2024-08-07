89518e449a961b8ad0426b997bd530c9f8acfcd8
Mateus Lopes <devmateustools@gmail.com>
2024-08-07 03:11:58 -0300

    feat: input validations

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,6 +1,37 @@
+'use client'
+import { zodResolver } from "@hookform/resolvers/zod";
 import { Box, Button, Stack, TextField, Typography } from "@mui/material";
+import { Controller, useForm } from "react-hook-form";
+import { z } from "zod";
+
+
+const loanSchema = z.object({
+  balance: z.preprocess(
+    (a) => parseInt(z.string().parse(a), 10),
+    z.number().positive()
+  ),
+  installments_value: z.preprocess(
+    (a) => parseInt(z.string().parse(a), 10),
+    z.number().positive()
+  ),
+  state_id: z.string(),
+  birth_date: z.string(),
+  cpf: z.string(),
+});
+
+type LoanProps = z.infer<typeof loanSchema>
+
 
 export default function Home() {
+
+  const { control, register, formState: { errors, isValid }, getValues, handleSubmit } = useForm<LoanProps>({
+    resolver: zodResolver(loanSchema)
+  });
+
+
+  const handleSimulate = (data: LoanProps) => {
+    console.log(data)
+  }
   return (
     <Box
       display="flex"
@@ -14,19 +45,55 @@ export default function Home() {
     >
       <Typography variant="h3" component="h3" color="grey.500" fontWeight="100" mb={8}>Simule e solicite o seu orçamento.</Typography>
 
-      <Stack component="form" spacing={2} direction="column" justifyContent="center" textAlign="center">
+      <Stack component="form" onSubmit={handleSubmit(handleSimulate)} spacing={2} direction="column" justifyContent="center" textAlign="center">
         <Typography variant="subtitle1" component="p" mb={2} fontWeight="700"> Preencha o formulário abaixo para simular </Typography>
         <Stack spacing={2} direction="column" width="55rem" bgcolor="white" px={4} py={8} borderRadius={2}>
-          <TextField label="CPF" variant="outlined" fullWidth />
-          <TextField label="UF" variant="outlined" fullWidth />
-          <TextField label="DATA DE NASCIMENTO" variant="outlined" fullWidth />
-          <TextField label="QUAL O VALOR DO EMPRESTIMO" variant="outlined" fullWidth />
-          <TextField label="QUAL VALOR DESEJA PAGAR POR MÊS?" variant="outlined" fullWidth />
-          <Button variant="contained" className="bg-amber-700">SIMULAR</Button>
+
+          <Controller
+            name="cpf"
+            control={control}
+            render={({ field: { onChange, value } }) => (
+              <TextField label="CPF" variant="outlined" fullWidth onChange={onChange} value={value} error={errors.cpf?.message !== undefined} helperText={errors.cpf?.message} />
+            )}
+          />
+
+          <Controller
+            name="state_id"
+            control={control}
+            render={({ field: { onChange, value } }) => (
+              <TextField label="UF" variant="outlined" type="number" fullWidth onChange={onChange} value={value} error={errors.state_id?.message !== undefined} helperText={errors.state_id?.message} />
+            )}
+          />
+
+
+          <Controller
+            name="birth_date"
+            control={control}
+            render={({ field: { onChange, value } }) => (
+              <TextField label="DATA DE NASCIMENTO" variant="outlined" fullWidth onChange={onChange} value={value} error={errors.birth_date?.message !== undefined} helperText={errors.birth_date?.message} />
+            )}
+          />
+
+
+          <Controller
+            name="balance"
+            control={control}
+            render={({ field: { onChange, value } }) => (
+              <TextField label="QUAL O VALOR DO EMPRESTIMO" variant="outlined" fullWidth onChange={onChange} value={value} error={errors.balance?.message !== undefined} helperText={errors.balance?.message} />
+            )}
+          />
+
+
+          <Controller
+            name="installments_value"
+            control={control}
+            render={({ field: { onChange, value } }) => (
+              <TextField label="QUAL VALOR DESEJA PAGAR POR MÊS?" variant="outlined" fullWidth onChange={onChange} value={value} error={errors.installments_value?.message !== undefined} helperText={errors.installments_value?.message} />
+            )}
+          />
+          <Button variant="contained" type="submit" className="bg-amber-700">SIMULAR</Button>
         </Stack>
       </Stack>
-
-
 
       <Box width="60rem" height="20rem" bgcolor="blue">
         eai
