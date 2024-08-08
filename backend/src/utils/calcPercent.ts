f635b9e1939d1aa86d2c44feeb28122525a2a8b3
Mateus Lopes <devmateustools@gmail.com>
2024-08-08 01:27:50 -0300

    fix

--- backend/src/utils/calcPercent.ts
+++ backend/src/utils/calcPercent.ts
@@ -1,5 +1,5 @@
 function calcPercent(balance: number, percent: number) {
-  return Number(balance.toFixed(2)) * Number(percent.toFixed(2)) / 100;
+  return Number(Number(balance).toFixed(2)) * Number(Number(percent).toFixed(2)) / 100;
 }
 
 export { calcPercent }
