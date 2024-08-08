88bf1ec20e19ce4c99399ed7840951b17c86df4f
Mateus Lopes <devmateustools@gmail.com>
2024-08-07 22:37:16 -0300

    feat: env configurations

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -1,11 +1,17 @@
 module.exports = {
-  username: "admin",
-  password: "admin",
-  database: "database",
-  host: "localhost",
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DATABASE,
+  host: process.env.POSTGRES_HOST,
   dialect: "postgres",
   repositoryMode: true,
   define: {
     timestamps: true
+  },
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
   }
 }
